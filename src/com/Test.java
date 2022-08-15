feb10c63f00acad389a52c112e136dded3cceff4
Qi <601407116@qq.com>
2022-08-15 17:57:17 +0800

    hot-fix的第一次修改

--- src/com/Test.java
+++ src/com/Test.java
@@ -3,7 +3,8 @@ package com;
 public class Test {
     public static void main(String[] args) {
         System.out.println("gitLearn_IDEA");
-        System.out.println("配置完毕2222222222");
-        System.out.println();
+        System.out.println("配置完毕33333");
+        System.out.println("hello,git");
+
     }
 }
