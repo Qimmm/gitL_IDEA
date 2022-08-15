c8b950227e8a87be6af9e598242df938bbd86559
Qi <601407116@qq.com>
2022-08-15 17:57:17 +0800

    第三个版本

--- src/com/Test.java
+++ src/com/Test.java
@@ -3,7 +3,9 @@ package com;
 public class Test {
     public static void main(String[] args) {
         System.out.println("gitLearn_IDEA");
-        System.out.println("配置完毕2222222222");
-        System.out.println();
+        System.out.println("配置完毕33333");
+        System.out.println();   //
+        //？？？
+        //为啥只有两个版本，我不能接受
     }
 }
